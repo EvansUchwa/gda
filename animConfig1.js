7f14a80e5cae1260e4ec6189d3b33223a1453347
EvansUchwa <58237783+EvansUchwa@users.noreply.github.com>
2021-12-23 18:52:25 +0100

    Add files via upload

---
+++ animConfig1.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+  "count": 6,
+  "size": {
+    "min": 193,
+    "max": 435,
+    "pulse": 0
+  },
+  "speed": {
+    "x": {
+      "min": 0.3,
+      "max": 0.3
+    },
+    "y": {
+      "min": 0.3,
+      "max": 0.3
+    }
+  },
+  "colors": {
+    "background": "#ffffff",
+    "particles": [
+      "#6bd6ff",
+      "#ffcb57",
+      "#ff333d"
+    ]
+  },
+  "blending": "overlay",
+  "opacity": {
+    "center": 1,
+    "edge": 0.1
+  },
+  "skew": 0,
+  "shapes": [
+    "c",
+    "s",
+    "t"
+  ]
+});
